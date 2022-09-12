9c84c17356a88b3929f88cd07aa48b3d41854451
Ev <dmitry_dtv@mail.ru>
2022-09-12 20:23:31 +0500

    220912

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type objectStruct struct {
+	Value int
+}
+
+func TestSomething(t *testing.T) {
+
+	// assert equality
+	assert.Equal(t, 123, 123, "they should be equal")
+
+	// assert inequality
+	assert.NotEqual(t, 123, 456, "they should not be equal")
+	object := objectStruct{21}
+	// assert for nil (good for errors)
+	assert.Nil(t, object)
+
+	// assert for not nil (good when you expect something)
+	if assert.NotNil(t, object) {
+
+		// now we know that object isn't nil, we are safe to make
+		// further assertions without causing any errors
+		assert.Equal(t, "Something", object.Value)
+
+	}
+
+}
